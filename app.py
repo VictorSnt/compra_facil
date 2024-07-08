41dafb2129e1c0675b7c29c1a1d2899f36e97997
victor <victoorsantos266@gmail.com>
2024-07-08 18:35:00 -0300

    feat: more ux

--- app.py
+++ app.py
@@ -1,111 +1,122 @@
-from services.fetch_services import (ProductFilter, StockUpdater,
-    SalesUpdate
-)
-from services.products_finder import ProductFinder
 from flask import Flask, jsonify, render_template, request, send_file
+from flask.views import MethodView
+from flask_cors import CORS
 from dotenv import load_dotenv
 from openpyxl import Workbook, load_workbook
-from flask_cors import CORS
 from pathlib import Path
 from os import getenv
 import json
+from services.fetch_services import ProductFilter, StockUpdater, SalesUpdate
+from services.products_finder import ProductFinder
 
-
+# Load environment variables
 load_dotenv()
 report_path = Path(getenv('STATIC') + 'report.json')
 spreadsheet_path = Path(getenv('STATIC') + 'pedido.xlsx')
+
+# Initialize Flask app
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def index_page():
-    finder = ProductFinder()
-    suppliers = finder.fetch_all_suppliers()
-    groups = finder.fetch_all_prod_groups()
-    families = finder.fetch_all_prod_family()
-    context = {'fornecedores': suppliers, 'groups': groups, 'families': families}
-    return render_template('index.html', **context)
+class IndexPage(MethodView):
+    def get(self):
+        finder = ProductFinder()
+        suppliers = finder.fetch_all_suppliers()
+        groups = finder.fetch_all_prod_groups()
+        families = finder.fetch_all_prod_family()
+        context = {'fornecedores': suppliers, 'groups': groups, 'families': families}
+        return render_template('index.html', **context)
 
-@app.route("/lista_compras")
-def list_compras():
-    data = request.args.get('data', False)
-    if data:
-        parsed_data = json.loads(data)
-        filtered_products = ProductFilter.filter_by_family_n_groups(parsed_data)
-        supplier_products = ProductFilter.filter_by_suppliers(parsed_data)
-        all_products = filtered_products + supplier_products
-        updated_products = StockUpdater.join_products_stock(all_products)
-        result = SalesUpdate.join_product_sales(updated_products)
-    
-        with open(report_path, 'w+') as file:
-            json.dump(result, file, indent=4)
-        return jsonify({'ok': True}), 200
-    else:
-        return jsonify({'ok': False}), 404
-
-@app.route("/informacoes")
-def reder_list():
-    if report_path.exists():
-        with open(report_path, 'r') as file:
-            result = json.load(file)
-        return render_template('prod_list.html', products=result)
-    else:
-        response = {'message': 'O relatorio não foi gerado, tente novamente'}
-        return jsonify(response)
-
-@app.route("/create_spreadsheet")
-def create_sheet():
-    data = request.args.get('data', False)
-    if data:
-        parsed_data: list = json.loads(data)
-        parsed_data.sort(key=lambda x: x['descricao'])
-        wb = Workbook()
-        sheet = wb.active
-        sheet['A1'] = 'codigo'
-        sheet['B1'] = 'descricao'
-        sheet['C1'] = 'compra'
-        sheet['D1'] = 'vl final' 
-        sheet['E1'] = 'vl sugestao' 
+class ListaCompras(MethodView):
+    def get(self):
+        data = request.args.get('data', False)
+        if data:
+            parsed_data = json.loads(data)
+            filtered_products = ProductFilter.filter_by_family_n_groups(parsed_data)
+            supplier_products = ProductFilter.filter_by_suppliers(parsed_data)
+            all_products = filtered_products + supplier_products
+            updated_products = StockUpdater.join_products_stock(all_products)
+            result = SalesUpdate.join_product_sales(updated_products)
         
-        for index, item in enumerate(parsed_data, start=2):
-            sheet[f'A{index}'] = item['codigo']
-            sheet[f'B{index}'] = item['descricao']
-            sheet[f'C{index}'] = item['compra']
-            sheet[f'D{index}'] = '' 
-            sheet[f'E{index}'] = '' 
-        
-        wb.save(spreadsheet_path)    
-        return jsonify({'ok': True}), 200
-    else:
-        return jsonify({'ok': False}), 404
+            with open(report_path, 'w+') as file:
+                json.dump(result, file, indent=4)
+            return jsonify({'ok': True}), 200
+        else:
+            return jsonify({'ok': False}), 404
 
-@app.route("/donwload_spreadsheet")
-def donwload_spreadsheet():
-    return send_file(spreadsheet_path)
+class Informacoes(MethodView):
+    def get(self):
+        if report_path.exists():
+            with open(report_path, 'r') as file:
+                result = json.load(file)
+            return render_template('prod_list.html', products=result)
+        else:
+            response = {'message': 'O relatorio não foi gerado, tente novamente'}
+            return jsonify(response)
 
-@app.route("/process_sheet", methods=['POST'])
-def process_sheet():
-    file = request.files.get('file')
-    
-    if file and file.filename.endswith('.xlsx'):
-        workbook = load_workbook(file)
-        sheet = workbook.active
-        
-        data = []
-        headers = [cell.value for cell in sheet[1]]
-        
-        for row in sheet.iter_rows(min_row=2, values_only=True):
-            row_data = {}
-            for header, value in zip(headers, row):
-                row_data[header] = value
-            data.append(row_data)
-        
-        return jsonify({'data': data}), 200
-    else:
-        return jsonify({'error': 'Arquivo inválido'}), 400
+class CreateSpreadsheet(MethodView):
+    def get(self):
+        data = request.args.get('data', False)
+        if data:
+            parsed_data = json.loads(data)
+            parsed_data.sort(key=lambda x: x['descricao'])
+            wb = Workbook()
+            sheet = wb.active
+            sheet['A1'] = 'codigo'
+            sheet['B1'] = 'descricao'
+            sheet['C1'] = 'compra'
+            sheet['D1'] = 'vl final'
+            sheet['E1'] = 'vl sugestao'
+            
+            for index, item in enumerate(parsed_data, start=2):
+                sheet[f'A{index}'] = item['codigo']
+                sheet[f'B{index}'] = item['descricao']
+                sheet[f'C{index}'] = item['compra']
+                sheet[f'D{index}'] = ''
+                sheet[f'E{index}'] = ''
+            
+            wb.save(spreadsheet_path)
+            return jsonify({'ok': True}), 200
+        else:
+            return jsonify({'ok': False}), 404
 
-@app.route('/carregar_planilha')
-def render_sheet():
-    return render_template('load_sheet.html')
+class DownloadSpreadsheet(MethodView):
+    def get(self):
+        return send_file(spreadsheet_path)
 
-app.run('0.0.0.0', 5000, True)
+class ProcessSheet(MethodView):
+    def post(self):
+        file = request.files.get('file')
+        
+        if file and file.filename.endswith('.xlsx'):
+            workbook = load_workbook(file)
+            sheet = workbook.active
+            
+            data = []
+            headers = [cell.value for cell in sheet[1]]
+            
+            for row in sheet.iter_rows(min_row=2, values_only=True):
+                row_data = {}
+                for header, value in zip(headers, row):
+                    row_data[header] = value
+                data.append(row_data)
+            
+            return jsonify({'data': data}), 200
+        else:
+            return jsonify({'error': 'Arquivo inválido'}), 400
+
+class CarregarPlanilha(MethodView):
+    def get(self):
+        return render_template('load_sheet.html')
+
+# Register the URLs with the classes
+app.add_url_rule('/', view_func=IndexPage.as_view('index_page'))
+app.add_url_rule('/lista_compras', view_func=ListaCompras.as_view('lista_compras'))
+app.add_url_rule('/informacoes', view_func=Informacoes.as_view('informacoes'))
+app.add_url_rule('/create_spreadsheet', view_func=CreateSpreadsheet.as_view('create_spreadsheet'))
+app.add_url_rule('/donwload_spreadsheet', view_func=DownloadSpreadsheet.as_view('donwload_spreadsheet'))
+app.add_url_rule('/process_sheet', view_func=ProcessSheet.as_view('process_sheet'), methods=['POST'])
+app.add_url_rule('/carregar_planilha', view_func=CarregarPlanilha.as_view('carregar_planilha'))
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000, True)
--- services/fetch_services.py
+++ services/fetch_services.py
@@ -24,8 +24,6 @@ class ProductFilter:
     @staticmethod
     def build_products_query(groups, families):
         group_values = ",".join([f"'{g['idgrupo']}'" for g in groups if groups])
-        print(groups)
-        print(families)
         family_values = ",".join([f"'{f['idfamilia']}'" for f in families if families])
 
         group_filter = f'prod.idgrupo in ({group_values})'
@@ -46,7 +44,8 @@ class ProductFilter:
         JOIN wshop.documen as doc on doc.iddocumento = item.iddocumento
         WHERE doc.idpessoa IN ({})  AND det.stdetalheativo = TRUE
         """
-        return query.format(ids) 
+        return query.format(ids)
+    
     
 class StockUpdater:
     @staticmethod
@@ -56,14 +55,14 @@ class StockUpdater:
 
         product_ids = [product['iddetalhe'] for product in products]
         stock_query = """
-        SELECT e.iddetalhe, e.qtestoque
-        FROM wshop.estoque e
-        INNER JOIN (
-            SELECT iddetalhe, MAX(dtreferencia) AS max_dtreferencia
-            FROM wshop.estoque
-            GROUP BY iddetalhe
-        ) AS latest_stock ON e.iddetalhe = latest_stock.iddetalhe AND e.dtreferencia = latest_stock.max_dtreferencia
-        WHERE e.iddetalhe IN ({})
+            SELECT e.iddetalhe, e.qtestoque
+            FROM wshop.estoque e
+            INNER JOIN (
+                SELECT iddetalhe, MAX(dtreferencia) AS max_dtreferencia
+                FROM wshop.estoque
+                GROUP BY iddetalhe
+            ) AS latest_stock ON e.iddetalhe = latest_stock.iddetalhe AND e.dtreferencia = latest_stock.max_dtreferencia
+            WHERE e.iddetalhe IN ({})
         """.format(','.join(['%s'] * len(product_ids)))
 
         stock_response = DatabaseHandler().execute_query(stock_query, tuple(product_ids))
@@ -76,6 +75,7 @@ class StockUpdater:
                 updated_products.append(product)
 
         return updated_products
+
 
 class OrderInfoBuilder:
 
@@ -103,30 +103,58 @@ class DocumentInfoBuilder:
             return {}
         
         doc_info_query = """
-            SELECT doc.iddocumento, doc.dtreferencia, doc.dtemissao, pes.nmpessoa, docitem.iddetalhe
-            FROM wshop.documen AS doc
-            JOIN wshop.docitem AS docitem ON docitem.iddocumento = doc.iddocumento
-            JOIN wshop.pessoas AS pes ON pes.idpessoa = doc.idpessoa 
-            WHERE (docitem.iddetalhe IN ({}))
-            AND doc.tpoperacao = 'C'
-            AND (docitem.iddetalhe, doc.dtreferencia) IN (
-                SELECT iddetalhe, dtreferencia
-                FROM (
-                    SELECT di.iddetalhe, di.dtreferencia, 
-                        ROW_NUMBER() OVER (PARTITION BY di.iddetalhe ORDER BY di.dtreferencia DESC) AS rn
-                    FROM wshop.documen AS d
-                    JOIN wshop.docitem AS di ON d.iddocumento = di.iddocumento
-                    WHERE di.iddetalhe IN ({})
+            SELECT
+    doc.iddocumento,
+    doc.dtreferencia,
+    doc.dtemissao,
+    pes.nmpessoa,
+    docitem.iddetalhe
+FROM
+    wshop.documen AS doc
+JOIN
+    wshop.docitem AS docitem ON docitem.iddocumento = doc.iddocumento
+JOIN
+    wshop.pessoas AS pes ON pes.idpessoa = doc.idpessoa
+WHERE
+    docitem.iddetalhe IN ({})
+    AND doc.tpoperacao = 'C'
+    AND (docitem.iddetalhe, doc.dtreferencia) IN (
+        SELECT
+            iddetalhe,
+            dtreferencia
+        FROM
+            (
+                SELECT
+                    di.iddetalhe,
+                    di.dtreferencia,
+                    ROW_NUMBER() OVER (PARTITION BY di.iddetalhe ORDER BY di.dtreferencia DESC) AS rn
+                FROM
+                    wshop.documen AS d
+                JOIN
+                    wshop.docitem AS di ON d.iddocumento = di.iddocumento
+                WHERE
+                    di.iddetalhe IN ({})
                     AND d.tpoperacao = 'C'
-                ) AS ranked
-                WHERE rn = 1
-            )
-            ORDER BY doc.dtreferencia DESC;
+            ) AS ranked
+        WHERE
+            rn <= 3
+    )
+    ORDER BY
+        docitem.iddetalhe,
+        doc.dtreferencia DESC;
+
         """
         formated_ids = ','.join(['%s'] * len(product_ids))
         doc_info_query = doc_info_query.format(formated_ids, formated_ids)
         doc_info_results = DatabaseHandler().execute_query(doc_info_query, tuple(product_ids + product_ids))
-        doc_info_map = {doc_info['iddetalhe']: doc_info for doc_info in doc_info_results}
+        doc_info_map = {}
+        for doc_info in doc_info_results:
+            if doc_info_map.get(doc_info['iddetalhe']):
+                doc_info_map[doc_info['iddetalhe']].append(doc_info)
+                continue
+            doc_info_map[doc_info['iddetalhe']] = []
+            doc_info_map[doc_info['iddetalhe']].append(doc_info)
+        
         return doc_info_map
     
 class SalesCalculator:
@@ -159,15 +187,18 @@ class SalesCalculator:
             dtref = dtreferencias.get(iddetalhe)
             if not dtref:
                 continue
+            else:
+                dtref = dtref[0]
             this_prod_sales = [sale['qtvenda'] for sale in sales_info if sale['qtvenda']]
             this_prod_sales_date = [sale['dtreferencia'] for sale in sales_info if sale['qtvenda']]
             all_sales_date = sorted(this_prod_sales_date, reverse=True)
             last_sale = all_sales_date[0].strftime('%d/%m/%Y') if all_sales_date else None
             top_3_sales = sum(list(sorted(this_prod_sales, key=lambda sale: sale, reverse=True))[:5]) / 5
-            period_sales =  [sale for sale in sales_info if sale['dtreferencia'] >= dtreferencias.get(iddetalhe)]
+            period_sales =  [sale for sale in sales_info if sale['dtreferencia'] >= dtref]
             only_sales = [period['qtvenda'] for period in period_sales if period['qtvenda']]
             calc_sales[iddetalhe] = {'total_sales': sum(only_sales), 'high_avg': top_3_sales, 'last_sale' :last_sale}
         return calc_sales
+
 
 class PaymentFluxFetcher:
     @staticmethod
@@ -185,6 +216,7 @@ class PaymentFluxFetcher:
             formated_flux[flux['iddocumento']].append(flux)
         return formated_flux
 
+
 class SalesUpdate:
     @staticmethod
     def join_product_sales(products: List[dict]) -> List[dict]:
@@ -193,15 +225,27 @@ class SalesUpdate:
 
         product_ids = [prod['iddetalhe'] for prod in products]
         doc_info_map = DocumentInfoBuilder.fetch_document_info(product_ids)
-        document_ids = [doc['iddocumento'] for doc in doc_info_map.values()]
+        document_ids = [doc[0]['iddocumento'] for doc in doc_info_map.values()]
         valid_products = SalesUpdate.filter_valid_products(products, doc_info_map)
         products_orders = OrderInfoBuilder.fetch_order_info(product_ids)
         payment_fluxes = PaymentFluxFetcher.fetch_payment_flux(document_ids)
-        dtreferencias = {doc['iddetalhe']: doc['dtreferencia'] for doc in doc_info_map.values()}
+        dtreferencias = {}
+        for doc_list in doc_info_map.values():
+            for doc in doc_list:
+                if doc.get('dtreferencia'):
+                    if dtreferencias.get(doc['iddetalhe']):
+                        dtreferencias[doc['iddetalhe']].append(doc['dtreferencia'])
+                        continue
+                    dtreferencias[doc['iddetalhe']] = []
+                    dtreferencias[doc['iddetalhe']].append(doc['dtreferencia'])
+        
         sales_quants = SalesCalculator.calculate_sales(product_ids, dtreferencias)
 
         updated_products = [
-            SalesUpdate.update_product(prod, doc_info_map, payment_fluxes, sales_quants, products_orders) 
+            SalesUpdate.update_product(
+                prod, doc_info_map, payment_fluxes, sales_quants, 
+                products_orders, dtreferencias
+            )
             for prod in valid_products
         ]
 
@@ -212,13 +256,20 @@ class SalesUpdate:
 
     @staticmethod
     def filter_valid_products(products: List[dict], doc_info_map: Dict) -> List[dict]:
-        return [prod for prod in products if prod['iddetalhe'] in doc_info_map]
+        return [prod for prod in products if prod['iddetalhe'] in doc_info_map.keys()]
 
     @staticmethod
-    def update_product(prod: dict, doc_info_map: Dict, payment_fluxes: Dict, sales_quants: Dict, products_orders: Dict) -> dict:
+    def update_product(
+            prod: dict, 
+            doc_info_map: Dict, 
+            payment_fluxes: Dict, 
+            sales_quants: Dict, 
+            products_orders: Dict,
+            dtreferencias: Dict[str, List[datetime]]
+        ) -> dict:
         product_id = prod['iddetalhe']
-        doc_info = doc_info_map.get(product_id)
-
+        doc_info = doc_info_map.get(product_id)[0]
+        dtreferencia = dtreferencias.get(product_id)
         if not doc_info:
             return prod
        
@@ -231,12 +282,16 @@ class SalesUpdate:
         prod['last_sale'] = sales_info.get('last_sale')
         last_sale_date = datetime.strptime(prod['last_sale'], '%d/%m/%Y') if prod['last_sale'] else datetime.now()
 
-        dtreferencia = doc_info['dtreferencia']
-        dtemissao = doc_info['dtemissao']
+     
+  
         nmfornecedor = doc_info['nmpessoa']
 
-        prod['dura_mes'] = SalesUpdate.calculate_dura_mes(prod, sales_quant, last_sale_date, dtreferencia)
+        calc_durames_response = SalesUpdate.calculate_dura_mes(prod, sales_quant, last_sale_date, dtreferencia)
+        dura_mes = calc_durames_response[0]
+        dtreferencia = calc_durames_response[1]
+        sales_days = calc_durames_response[2]
         shipping_days = 7
+        prod['dura_mes'] = dura_mes
         prazo = SalesUpdate.calculate_prazo(payment_flux)
 
         prod.update({
@@ -244,10 +299,10 @@ class SalesUpdate:
             'sales': sales_quant,
             'shipping_days': shipping_days,
             'supplier': nmfornecedor,
-            'stock_min': SalesUpdate.calculate_stock_min(prod, high_avg, sales_quant, shipping_days, last_sale_date, dtreferencia),
-            'sugestion': SalesUpdate.calculate_sugestion(sales_quant, prod['dura_mes'], prod, last_sale_date, dtreferencia, shipping_days)
+            'stock_min': SalesUpdate.calculate_stock_min(high_avg, sales_quant, shipping_days, sales_days),
+            'sugestion': SalesUpdate.calculate_sugestion(sales_quant, prod['dura_mes'], shipping_days, sales_days)
         })
-        prod['stock_max'] = SalesUpdate.calculate_stock_max(prod, prod['stock_min'], sales_quant, last_sale_date, dtreferencia)
+        prod['stock_max'] = SalesUpdate.calculate_stock_max(prod['stock_min'], sales_quant, sales_days)
         prod['sugestion'] = floor(prod['sugestion'])
         
         if prod['qtestoque'] < prod['stock_min']:
@@ -256,7 +311,6 @@ class SalesUpdate:
         if prod['qtestoque'] >= prod['stock_max']:
             prod['sugestion'] = 0
         
-        sales_days = (last_sale_date - dtreferencia).days if prod['qtestoque'] <= 0 else (datetime.now() - dtreferencia).days
         if sales_quant and sales_days > 0:
             prod['dura_mes'] = (prod['qtestoque'] > prod['sugestion'])
 
@@ -270,11 +324,19 @@ class SalesUpdate:
         return prod
 
     @staticmethod
-    def calculate_dura_mes(prod: dict, sales_quant: int, last_sale_date: datetime, dtreferencia: datetime) -> bool:
+    def calculate_dura_mes(prod: dict, sales_quant: int, last_sale_date: datetime, dtreferencia: List[datetime]) -> List:
+        
+        sales_days = 0
+        for date in dtreferencia: 
+            sales_days = (last_sale_date - date).days if prod['qtestoque'] <= 0 else (datetime.now() - date).days
+            if sales_days >= 7:
+                break
+        dura_mes = bool(sales_days <= 0 or (prod['qtestoque'] > ((sales_quant / sales_days) * 55)))
+        
         if not sales_quant:
-            return True
-        sales_days = (last_sale_date - dtreferencia).days if prod['qtestoque'] <= 0 else (datetime.now() - dtreferencia).days 
-        return sales_days <= 0 or (prod['qtestoque'] > ((sales_quant / sales_days) * 55))
+            dura_mes = True
+        
+        return [dura_mes, date, sales_days]
 
     @staticmethod
     def calculate_prazo(payment_flux: list) -> list:
@@ -283,20 +345,15 @@ class SalesUpdate:
         return 'N/A'
 
     @staticmethod
-    def calculate_sugestion(sales_quant: int, dura_mes: bool, prod, last_sale_date, dtreferencia, shipping_days) -> float:
+    def calculate_sugestion(sales_quant: int, dura_mes: bool, shipping_days, sales_days) -> float:
         if dura_mes:
-            return 0
-        sales_days = (last_sale_date - dtreferencia).days if prod['qtestoque'] <= 0 else (datetime.now() - dtreferencia).days 
+            return 0 
         daily_avg = (sales_quant / sales_days) * 0.75
         return daily_avg * (55 + shipping_days)
     
     @staticmethod
-    def calculate_stock_min(
-            prod, high_avg: float, sales_quant: int, shipping_days: int, 
-            last_sale_date: datetime, dtreferencia: datetime
-        ) -> int:
+    def calculate_stock_min(high_avg: float, sales_quant: int, shipping_days: int, sales_days) -> int:
         try:
-            sales_days = (last_sale_date - dtreferencia).days if prod['qtestoque'] <= 0 else (datetime.now() - dtreferencia).days 
             daily_avg = (sales_quant / sales_days) * 0.75
             possible_delay = 1.8
             quotation_period = 2
@@ -306,9 +363,9 @@ class SalesUpdate:
             return high_avg
 
     @staticmethod
-    def calculate_stock_max(prod, stock_min: int, sales_quant: int, last_sale_date: datetime, dtreferencia: datetime) -> int:
+    def calculate_stock_max(stock_min: int, sales_quant: int, sales_days) -> int:
         try:
-            sales_days = (last_sale_date - dtreferencia).days if prod['qtestoque'] <= 0 else (datetime.now() - dtreferencia).days 
+            
             daily_avg = (sales_quant / sales_days) * 0.75
             stock_max = stock_min + daily_avg * 55
             return max(3, int(stock_max))
